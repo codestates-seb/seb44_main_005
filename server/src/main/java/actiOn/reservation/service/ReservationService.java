61f269bfce449f2afef72096a874c81c2fab95c3
isu-nice <isu010614@naver.com>
2023-03-22 10:00:23 +0900

    [fix] 예약 전체 수정 (#101)

--- server/src/main/java/actiOn/reservation/service/ReservationService.java
+++ server/src/main/java/actiOn/reservation/service/ReservationService.java
@@ -5,7 +5,7 @@ import actiOn.exception.BusinessLogicException;
 import actiOn.exception.ExceptionCode;
 import actiOn.item.dto.ItemDto;
 import actiOn.item.entity.Item;
-import actiOn.item.repository.ItemRepository;
+import actiOn.item.service.ItemService;
 import actiOn.member.entity.Member;
 import actiOn.member.service.MemberService;
 import actiOn.reservation.entity.Reservation;
@@ -29,26 +29,28 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final StoreService storeService;
     private final MemberService memberService;
-    private final ItemRepository itemRepository;
+    private final ItemService itemService;
     private final ReservationItemRepository reservationItemRepository;
 
     @Transactional(propagation = Propagation.REQUIRED)
     public void postReservation(Long storeId, Reservation reservation, List<ReservationItem> reservationItems) {
-        //Todo store 존재하는지 여부 확인 -> 예외처리 리팩토링 필요
         Store store = storeService.findStoreByStoreId(storeId);
         reservation.setStore(store);
 
-        //예약 날짜 유효성 검사 -> 오늘 기준 이전 날짜가 오면 에러 발생
+        //예약 날짜 유효성 검사
         validateReservationDate(reservation.getReservationDate());
 
         //로그인한 유저의 정보를 reservation에 담기
-        String loginUserEmail = AuthUtil.getCurrentMemberEmail();
-        Member member = memberService.findMemberByEmail(loginUserEmail);
+        String email = AuthUtil.getCurrentMemberEmail();
+        Member member = memberService.findMemberByEmail(email);
         reservation.setMember(member);
 
-        //reqReservation에 있는 상품 id가 존재하는지 확인 후 reservationItem 저장
-        //List<ReservationItem> saveReservationItems = createReservationItem(reservationItems);
-        reservation.setReservationItems(reservationItems);
+        // reservationItem 저장
+        List<ReservationItem> newReservationItems = generateReservationItem(reservation, reservationItems);
+        reservation.setReservationItems(newReservationItems);
+
+        // 입력된 총 금액과 총 예약 가격이 동일한지 검증
+        validateTotalPrice(newReservationItems, reservation.getTotalPrice());
 
         //예약 정보 저장
         reservationRepository.save(reservation);
@@ -72,6 +74,15 @@ public class ReservationService {
         reservationRepository.save(findReservation);
     }
 
+    // 예약 검증 및 조회
+    @Transactional(readOnly = true)
+    public Reservation getReservations(Long reservationId) {
+        Reservation reservation = findReservation(reservationId);
+        verifyReservationMember(reservation.getMember());
+
+        return reservation;
+    }
+
     @Transactional(propagation = Propagation.REQUIRED)
     public void cancelReservation(Long reservationId) {
         Reservation findReservation = findReservation(reservationId);
@@ -80,80 +91,50 @@ public class ReservationService {
         verifyReservationMember(findReservation.getMember());
 
         //예약 대기 -> 예약 취소
-        findReservation.setReservationStatus(Reservation.ReservationStatus.RESERVATION_CANCLE);
+        findReservation.setReservationStatus(Reservation.ReservationStatus.RESERVATION_CANCEL);
         reservationRepository.delete(findReservation);
 
         //Todo 예약 취소 -> 환불
     }
 
-    // 로그인한 회원과 reservation member 일치하는지 확인
-    private void verifyReservationMember(Member reservationMember) {
-        String loginUserEmail = AuthUtil.getCurrentMemberEmail();
-        Member findMember = memberService.findMemberByEmail(loginUserEmail);
-
-        if (!findMember.equals(reservationMember)) {
-            throw new BusinessLogicException(ExceptionCode.RESERVATION_MEMBER_NOT_FOUND);
-        }
-    }
-
-    @Transactional(readOnly = true)
-    public Reservation getReservations(Long reservationId) {
-        Reservation reservation = findReservation(reservationId);
-        return reservation;
-    }
-
-    //예약 찾기
-    private Reservation findReservation(Long reservationId) {
-        return reservationRepository.findById(reservationId)
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.RESERVATION_NOT_FOUND));
-    }
-
-    // 예약 날짜 검증 메서드
-    private void validateReservationDate(LocalDate reservationDate) {
-        if (reservationDate != null) {
-            LocalDate today = LocalDate.now();
-            if (reservationDate.isBefore(today)) {
-                throw new IllegalArgumentException("예약 날짜는 이전 날짜에 적용할 수 없습니다.");
-            }
-        }
-    }
-
     //예약 상품 생성 및 저장 메서드
-    private List<ReservationItem> createReservationItem(Reservation reservation) {
-        List<ReservationItem> saveReservationItemList = new ArrayList<>();
-        List<ReservationItem> reservationItemList = reservation.getReservationItems();
+    private List<ReservationItem> generateReservationItem(Reservation reservation, List<ReservationItem> reservationItems) {
+        List<ReservationItem> newReservationItems = new ArrayList<>();
 
-        for (ReservationItem reservationItem : reservationItemList) {
-            Long itemId = reservationItem.getItem().getItemId();
-            //itemId로 해당 상품을 찾았음
-            Item item = itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
-            //티켓 valid 검사
-            item.validateTicketCount(reservationItem.getTicketCount());
-
-            //ReservationItem 저장
-            ReservationItem saveReservationItem = new ReservationItem();
-            saveReservationItem.setTicketCount(reservationItem.getTicketCount());
-            saveReservationItem.setItem(item);
-            saveReservationItem.setReservation(reservation);
-
-            saveReservationItemList.add(saveReservationItem);
-            //예약상품 저장
-            reservationItemRepository.save(saveReservationItem);
+        for (ReservationItem reservationItem : reservationItems) {
+            reservationItem.setReservation(reservation);
+            newReservationItems.add(reservationItem);
         }
-        //총 금액과 sumTicketCount의 가격이 동일한지 검증
-        validateTotalPrice(saveReservationItemList, reservation.getTotalPrice());
 
-        return saveReservationItemList;
+        return reservationItemRepository.saveAll(newReservationItems);
     }
 
-    //예약 금액 검증 메서드
-    private void validateTotalPrice(List<ReservationItem> reservationItems, int totalPrice) {
-        int sumTicketCount = 0;
-        for (ReservationItem reservationItem : reservationItems) {
-            sumTicketCount += reservationItem.getTicketCount() * reservationItem.getItem().getPrice();
-        }
-        if (totalPrice != sumTicketCount) {
-            throw new IllegalArgumentException("예약하신 총 금액과 각 티켓 값의 총 금액이 일치하지 않습니다.");
+    public List<ItemDto> findItemsByStoreIdAndDate(long storeId, LocalDate date) {
+        try {
+            //Todo 예외처리하기
+            List<ItemDto> itemDtos = new ArrayList<>();
+            Store findStore = storeService.findStoreByStoreId(storeId);
+            List<Item> findItems = findStore.getItems();
+            Map<Long, Integer> reservationTickets = reservationTicketCount(findStore, date);
+            for (Item item : findItems) {
+                int reservationTicketCount = // 예약된 티켓이 없다면 null로 나오므로, null과 int는 연산이 불가능하므로, int로 변환
+                        reservationTickets.containsKey(item.getItemId())
+                                ? reservationTickets.get(item.getItemId()) : 0;
+
+                int remainingTicketCount = item.getTotalTicket() - reservationTicketCount;
+                if (remainingTicketCount < 0) remainingTicketCount = 0;
+                ItemDto itemDto = new ItemDto(
+                        item.getItemId(),
+                        item.getItemName(),
+                        item.getTotalTicket(),
+                        item.getPrice(),
+                        remainingTicketCount
+                );
+                itemDtos.add(itemDto);
+            }
+            return itemDtos;
+        } catch (Exception e) {
+            return null;
         }
     }
 
@@ -180,32 +161,44 @@ public class ReservationService {
         return remainingTicketInfo;
     }
 
-    public List<ItemDto> findItemsByStoreIdAndDate(long storeId, LocalDate date) {
-        try{
-            //Todo 예외처리하기
-            List<ItemDto> itemDtos = new ArrayList<>();
-            Store findStore = storeService.findStoreByStoreId(storeId);
-            List<Item> findItems = findStore.getItems();
-            Map<Long,Integer> reservationTickets = reservationTicketCount(findStore,date);
-            for (Item item : findItems) {
-                int reservationTicketCount = // 예약된 티켓이 없다면 null로 나오므로, null과 int는 연산이 불가능하므로, int로 변환
-                        reservationTickets.containsKey(item.getItemId())
-                                ? reservationTickets.get(item.getItemId()) : 0;
+    // 로그인한 회원과 reservation member 일치하는지 확인
+    private Member verifyReservationMember(Member reservationMember) {
+        String loginUserEmail = AuthUtil.getCurrentMemberEmail();
+        Member findMember = memberService.findMemberByEmail(loginUserEmail);
 
-                int remainingTicketCount = item.getTotalTicket()-reservationTicketCount;
-                if (remainingTicketCount <0) remainingTicketCount=0;
-                ItemDto itemDto = new ItemDto(
-                        item.getItemId(),
-                        item.getItemName(),
-                        item.getTotalTicket(),
-                        item.getPrice(),
-                        remainingTicketCount
-                );
-                itemDtos.add(itemDto);
-            }
-            return itemDtos;
-        }catch (Exception e) {
-            return null;
+        if (!findMember.equals(reservationMember)) {
+            throw new BusinessLogicException(ExceptionCode.RESERVATION_MEMBER_NOT_FOUND);
+        }
+
+        return findMember;
+    }
+
+    //예약 찾기
+    private Reservation findReservation(Long reservationId) {
+        return reservationRepository.findById(reservationId)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.RESERVATION_NOT_FOUND));
+    }
+
+    // 예약 날짜 검증
+    private void validateReservationDate(LocalDate reservationDate) {
+        // 오늘 기준 이전 날짜가 오면 에러 발생
+        LocalDate today = LocalDate.now();
+
+        if (reservationDate.isBefore(today)) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_RESERVATION_DATE);
+        }
+    }
+
+    // 예약 금액 검증 메서드
+    private void validateTotalPrice(List<ReservationItem> reservationItems, int totalPrice) {
+        int sumTotalPrice = 0;
+        for (ReservationItem reservationItem : reservationItems) {
+            int reservationItemPrice = reservationItem.getItem().getPrice() * reservationItem.getTicketCount();
+            sumTotalPrice += reservationItemPrice;
+        }
+
+        if (totalPrice != sumTotalPrice) {
+            throw new BusinessLogicException(ExceptionCode.RESERVATION_TOTAL_PRICE_MISMATCH);
         }
     }
 }
