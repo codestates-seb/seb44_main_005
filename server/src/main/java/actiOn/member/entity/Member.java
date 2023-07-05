04c32e705f1999eceb5733a6f639a550606d5294
isu-nice <isu010614@naver.com>
2023-07-05 21:59:20 +0900

    [feat] Member 엔티티 설계 (#3)

--- server/src/main/java/actiOn/member/entity/Member.java
+++ server/src/main/java/actiOn/member/entity/Member.java
@@ -1,4 +1,53 @@
 package actiOn.member.entity;
 
-public class Member {
+import actiOn.business.entity.Business;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member implements Principal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Business> businesses = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Wish> wishList = new ArrayList<>();
+
+    public void addBusinesses(Business business) {
+        this.businesses.add(business);
+        if (business.getMember() != this) {
+            business.setMember(this);
+        }
+    }
+
+    public void addWishList(Wish wish) {
+        this.wishList.add(wish);
+        if (wish.getMember() != this) {
+            wish.setMember(this);
+        }
+    }
 }
