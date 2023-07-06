ceeb3f901460ef1f0d9dbc5524fdc47d8808c51c
isu-nice <isu010614@naver.com>
2023-07-06 16:50:33 +0900

    [feat] 예외처리 로직 추가 (#10)

---
+++ server/src/main/java/actiOn/exception/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,90 @@
+package actiOn.exception.advice;
+
+import actiOn.exception.BusinessLogicException;
+import actiOn.exception.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.net.BindException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    // 포트 중복 사용 등
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBindException(BindException e) {
+        final ErrorResponse response = ErrorResponse.of(e);
+
+        return response;
+    }
+
+    // 필드, 외래 키, db, null 제약 조건 위반할 시
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+
+        return response;
+    }
+
+    // businessLogic 사용자 정의 에러 발생 시
+    @ExceptionHandler
+    public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getExceptionCode().getStatus()));
+    }
+
+    // 특정 URL에 HTTP 메서드 요청이 잘못된 경우 등
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e) {
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED);
+
+        return response;
+    }
+
+    // 클라이언트가 잘못된(혹은 비어있는) 형식의 요청 본문을 전송한 경우
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException e) {
+
+        final ErrorResponse response = ErrorResponse
+                .of(HttpStatus.BAD_REQUEST, "요청/응답 body의 형식이 잘못되었습니다.");
+
+        return response;
+    }
+
+    // 파라미터 누락/형식 오류 등
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException e) {
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
+
+        return response;
+    }
+
+    // 핸들되지 않는 나머지 예외처리
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleUnableHandlingException(Exception e) {
+        log.error("# handle Exception", e);
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return response;
+    }
+}
