2c8c1bb3d57b7dce4134c74573c10c2ed35f7a61
chen4023 <ads4023@naver.com>
2023-07-06 15:14:04 +0900

    [init]:eslint작업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,14 @@
-import tw from "tailwind-styled-components"
+import tw from 'tailwind-styled-components';
+import React from 'react';
 
 function App() {
-  return (
-    <Test>안녕하세요</Test>
-  )
+  return <Test>안녕하세요</Test>;
 }
 
-export default App
+export default App;
 
 const Test = tw.section`
   text-3xl
   font-bold
   underline
-`
+`;
