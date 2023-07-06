804c4a57ff0b6faf2e6da42ba91b10684e792352
isu-nice <isu010614@naver.com>
2023-07-06 11:20:28 +0900

    [feat] config

---
+++ server/src/main/java/actiOn/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package actiOn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+//@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        System.out.println("filterChain");
+        http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()
+                .cors(Customizer.withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() /// Todo URI 권한 레벨 설정
+                );
+        return http.build() ;
+    }
+}
