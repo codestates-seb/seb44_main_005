18ff33aae66784b67244c43cf753948b064b1d77
isu-nice <isu010614@naver.com>
2023-07-07 15:32:37 +0900

    [feat] password encoder 추가 (#3)

--- server/src/main/java/actiOn/config/SecurityConfiguration.java
+++ server/src/main/java/actiOn/config/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,5 +29,10 @@ public class SecurityConfiguration {
                         .anyRequest().permitAll() /// Todo URI 권한 레벨 설정
                 );
         return http.build() ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
