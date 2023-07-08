b8a0ead26548da44202c46abd30712f42c5b1ae2
isu-nice <isu010614@naver.com>
2023-07-08 16:44:08 +0900

    [feat] 리소스 설정

--- server/src/main/java/actiOn/config/SecurityConfiguration.java
+++ server/src/main/java/actiOn/config/SecurityConfiguration.java
@@ -2,10 +2,13 @@ package actiOn.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -13,30 +16,52 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
-//@EnableWebSecurity
-public class SecurityConfiguration {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .headers().frameOptions().sameOrigin()
+
                 .and()
                 .csrf().disable()
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .cors(withDefaults())
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll() /// Todo URI 권한 레벨 설정
-                );
-        return http.build() ;
+
+                .exceptionHandling()
+
+                .and()
+                .apply(new CustomFilterConfigurer())
+
+                .and()
+                .authorizeRequests().anyRequest().permitAll();
+
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().permitAll() /// Todo URI 권한 레벨 설정
+//                );
     }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            super.configure(builder);
+        }
+    }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH","DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
         configuration.addAllowedHeader("*"); // 모든 헤더 적용
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
