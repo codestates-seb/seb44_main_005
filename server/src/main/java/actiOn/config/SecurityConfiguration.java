d6a622d46eb9534d10cb818938e0c6d90b82c69f
isu-nice <isu010614@naver.com>
2023-07-09 14:22:23 +0900

    [feat] security 구성 (#46)

--- server/src/main/java/actiOn/config/SecurityConfiguration.java
+++ server/src/main/java/actiOn/config/SecurityConfiguration.java
@@ -1,10 +1,21 @@
 package actiOn.config;
 
+import actiOn.auth.filter.JwtAuthenticationFilter;
+import actiOn.auth.filter.JwtVerificationFilter;
+import actiOn.auth.handler.MemberAccessDeniedHandler;
+import actiOn.auth.handler.MemberAuthenticationEntryPoint;
+import actiOn.auth.handler.MemberAuthenticationFailureHandler;
+import actiOn.auth.handler.MemberAuthenticationSuccessHandler;
+import actiOn.auth.provider.TokenProvider;
+import actiOn.auth.utils.MemberAuthorityUtil;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,30 +25,80 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfiguration {
+    private final TokenProvider tokenProvider;
+    private final MemberAuthorityUtil authorityUtil;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .headers().frameOptions().sameOrigin()
+
                 .and()
                 .csrf().disable()
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .cors(Customizer.withDefaults())
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
+                .accessDeniedHandler(new MemberAccessDeniedHandler())
+
+                .and()
+                .apply(new CustomFilterConfigurer())
+
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll() /// Todo URI 권한 레벨 설정
                 );
-        return http.build() ;
+        return http.build();
     }
+
+    // JwtAuthenticationFilter 구성하는 클래스
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager =
+                    builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter =
+                    new JwtAuthenticationFilter(authenticationManager);
+            jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler(tokenProvider));
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
+
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(tokenProvider, authorityUtil);
+
+            // Spring Security Filter Chain에 추가
+            builder.addFilter(jwtAuthenticationFilter)
+                    // TODO oauth2 필터 추가
+                    .addFilterAfter(jwtVerificationFilter, JwtVerificationFilter.class);
+        }
+    }
+
+    // 구체적인 CORS 정책 설정
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH","DELETE"));
-        configuration.addAllowedHeader("*"); // 모든 헤더 적용
+
+        configuration.setAllowedOrigins(
+                Arrays.asList(
+                        "http://localhost:3000")
+                // TODO S3 엔드포인트 추가 ""
+        );
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(2000L);
+        configuration.setAllowedHeaders(Arrays.asList("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"));
+        configuration.setExposedHeaders(Arrays.asList("authorization", "refresh"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
