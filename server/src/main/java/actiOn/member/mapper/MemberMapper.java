13c197a9242e2d9b88eacb4a6d6d922fed511eb6
isu-nice <isu010614@naver.com>
2023-07-10 00:21:13 +0900

    [feat] 마이페이지 - 회원 정보 조회 mapper 구현 (#32)

--- server/src/main/java/actiOn/member/mapper/MemberMapper.java
+++ server/src/main/java/actiOn/member/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package actiOn.member.mapper;
 
 import actiOn.member.dto.MemberPatchDto;
 import actiOn.member.dto.MemberPostDto;
+import actiOn.member.dto.MemberResponseDto;
 import actiOn.member.entity.Member;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
@@ -12,4 +13,13 @@ public interface MemberMapper {
     Member memberPostSignupDtoToMember(MemberPostDto.Signup requestBody);
 
     Member memberPatchDtoToMember(MemberPatchDto requestBody);
+
+    default MemberResponseDto memberToMemberResponseDto(Member member) {
+        return MemberResponseDto.builder()
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .phoneNumber(member.getPhoneNumber())
+                .profileImg(member.getProfileImg().getLink())
+                .build();
+    }
 }
