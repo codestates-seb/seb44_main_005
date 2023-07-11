40aebd1eae04d0e30e1b4ab010a3f5889476c9af
isu-nice <isu010614@naver.com>
2023-07-11 10:02:43 +0900

    [feat] 결제 서비스 구성 (#98)

--- server/src/main/java/actiOn/payment/service/PaymentService.java
+++ server/src/main/java/actiOn/payment/service/PaymentService.java
@@ -1,4 +1,40 @@
 package actiOn.payment.service;
 
+import actiOn.exception.BusinessLogicException;
+import actiOn.exception.ExceptionCode;
+import actiOn.member.entity.Member;
+import actiOn.member.service.MemberService;
+import actiOn.payment.entity.Payment;
+import actiOn.payment.repository.PaymentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
 public class PaymentService {
+    private final PaymentRepository paymentRepository;
+    private final MemberService memberService;
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public Payment requestPayment(String email, Payment payment) {
+        Member customer = memberService.findMemberByEmail(email);
+
+        // 총 결제 금액이 0 이하인 경우
+        if (payment.getTotalAmount() <= 0) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_PAYMENT_AMOUNT);
+        }
+
+        payment.setCustomer(customer);
+        return paymentRepository.save(payment);
+    }
+
+    public void sendPaymentRequest() {
+
+    }
+
+    public void handlePaymentResponse(Payment payment) {
+
+    }
 }
