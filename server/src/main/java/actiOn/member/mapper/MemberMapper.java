2b68cfb07e089ae1f4deec0fc5c185daa7318e42
isu-nice <isu010614@naver.com>
2023-07-13 00:28:18 +0900

    [feat] 파트너 정보 조회 DTO 구현 (#73)

--- server/src/main/java/actiOn/member/mapper/MemberMapper.java
+++ server/src/main/java/actiOn/member/mapper/MemberMapper.java
@@ -1,9 +1,6 @@
 package actiOn.member.mapper;
 
-import actiOn.member.dto.MemberPatchDto;
-import actiOn.member.dto.MemberPostDto;
-import actiOn.member.dto.MemberResponseDto;
-import actiOn.member.dto.PartnerResponseDto;
+import actiOn.member.dto.*;
 import actiOn.member.entity.Member;
 import actiOn.store.entity.Store;
 import org.mapstruct.Mapper;
@@ -28,8 +25,33 @@ public interface MemberMapper {
                 .build();
     }
 
-    default PartnerResponseDto partnerToPartnerStoreResponseDto(Member partner) {
+    // 파트너 정보 DTO
+    default PartnerResponseDto partnerToPartnerResponseDto(Member partner) {
         PartnerResponseDto.PartnerResponseDtoBuilder builder = PartnerResponseDto.builder();
+
+        builder.stores(
+                storesToPartnerResponseDtos(partner.getStores())
+        );
+
+        return builder.build();
+    }
+
+    default List<PartnerResponseDto.PartnerStoreDto> storesToPartnerResponseDtos(List<Store> stores) {
+        List<PartnerResponseDto.PartnerStoreDto> partnerDtos = new ArrayList<>();
+
+        for (Store store : stores) {
+            partnerDtos.add(storeToPartnerResponseDto(store));
+        }
+
+        return partnerDtos;
+    }
+
+    PartnerResponseDto.PartnerStoreDto storeToPartnerResponseDto(Store store);
+
+
+    // 파트너 업체 DTO
+    default PartnerStoreResponseDto partnerToPartnerStoreResponseDto(Member partner) {
+        PartnerStoreResponseDto.PartnerStoreResponseDtoBuilder builder = PartnerStoreResponseDto.builder();
 
         builder.stores(
                 storesToPartnerStoreResponseDtos(partner.getStores())
@@ -38,9 +60,9 @@ public interface MemberMapper {
         return builder.build();
     }
 
-    default List<PartnerResponseDto.PartnerStoreDto> storesToPartnerStoreResponseDtos(List<Store> stores) {
+    default List<PartnerStoreResponseDto.PartnerStoreDto> storesToPartnerStoreResponseDtos(List<Store> stores) {
+        List<PartnerStoreResponseDto.PartnerStoreDto> partnerStoreDtos = new ArrayList<>();
 
-        List<PartnerResponseDto.PartnerStoreDto> partnerStoreDtos = new ArrayList<>();
         for (Store store : stores) {
             partnerStoreDtos.add(storeToPartnerStoreResponseDto(store));
         }
@@ -48,5 +70,6 @@ public interface MemberMapper {
         return partnerStoreDtos;
     }
 
-    PartnerResponseDto.PartnerStoreDto storeToPartnerStoreResponseDto(Store store);
+    PartnerStoreResponseDto.PartnerStoreDto storeToPartnerStoreResponseDto(Store store);
+
 }
