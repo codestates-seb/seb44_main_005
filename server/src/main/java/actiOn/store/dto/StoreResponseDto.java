39ea7bbc3ceb314c8535ba3660f25ca2fbad340f
isu-nice <isu010614@naver.com>
2023-07-12 15:14:06 +0900

    [feat] 파트너 업체 DTO 관련 (#73)

--- server/src/main/java/actiOn/store/dto/StoreResponseDto.java
+++ server/src/main/java/actiOn/store/dto/StoreResponseDto.java
@@ -1,4 +1,37 @@
 package actiOn.store.dto;
 
+
+import actiOn.item.dto.ItemDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
 public class StoreResponseDto {
+    private String storeName;
+    private String category;
+    private String body;
+    private double latitude;
+    private double longitude;
+    private String kakao;
+    private String contact;
+    private String address;
+    private boolean isLike = false;
+    private String profileImg;
+    private LocalDateTime createdAt;
+    private List<ItemDto> items;
+    private List<String> storeImages;
+
+    public boolean getIsLike() {
+        return isLike;
+    }
+
+    public void setIsLike(boolean isLike) {
+        isLike = isLike;
+    }
 }
--- server/src/main/java/actiOn/store/mapper/StoreMapper.java
+++ server/src/main/java/actiOn/store/mapper/StoreMapper.java
@@ -4,7 +4,6 @@ package actiOn.store.mapper;
 import actiOn.item.entity.Item;
 import actiOn.store.dto.StorePatchDto;
 import actiOn.store.dto.StorePostDto;
-import actiOn.store.dto.StoreResponseDto;
 import actiOn.store.entity.Store;
 import org.mapstruct.Mapper;
 
@@ -13,8 +12,9 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface StoreMapper {
     Store storePostDtoToStore(StorePostDto storePostDto);
+
     Store storePatchDtoToStore(StorePatchDto storePatchDto);
-    StoreResponseDto storeToStoreResponseDto(Store store);
+    // StoreResponseDto storeToStoreResponseDto(Store store);
 
     List<Item> storePostDtoItemsToItemEntities(List<Item> itemList);
 }
