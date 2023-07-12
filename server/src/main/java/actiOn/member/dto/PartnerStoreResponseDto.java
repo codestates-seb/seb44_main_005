b6674a12b50f0f17afef6f7f9ef23f6fa152eaf6
isu-nice <isu010614@naver.com>
2023-07-13 00:29:12 +0900

    [feat] 파트너 관리 업체 조회 DTO (#73)

---
+++ server/src/main/java/actiOn/member/dto/PartnerStoreResponseDto.java
@@ -0,0 +1,22 @@
+package actiOn.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Builder
+@Setter
+@Getter
+public class PartnerStoreResponseDto {
+    private List<PartnerStoreDto> stores;
+
+    @Builder
+    @Getter
+    @Setter
+    public static class PartnerStoreDto {
+        private String storeImage;
+        private String storeName;
+    }
+}
