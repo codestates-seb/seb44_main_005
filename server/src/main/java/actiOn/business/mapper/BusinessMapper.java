dca444c1b872e84d556c4a7ee2507c16c632f859
isu-nice <isu010614@naver.com>
2023-07-12 13:39:57 +0900

    [feat] 비즈니스 등록 mapper (#73)

--- server/src/main/java/actiOn/business/mapper/BusinessMapper.java
+++ server/src/main/java/actiOn/business/mapper/BusinessMapper.java
@@ -1,4 +1,12 @@
 package actiOn.business.mapper;
 
-public class BusinessMapper {
+import actiOn.business.dto.BusinessDto;
+import actiOn.business.entity.Business;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface BusinessMapper {
+    Business BusinessPostDtoToBusiness(BusinessDto.Post requestBody);
 }
