cfb3b6f5a25aec42b55fc4af5235f07d901ad9a2
kminvita <vita_mink@daum.net>
2023-07-12 18:02:51 +0900

    [design] 스타일 분리

--- client/src/pages/ReservationCheck.tsx
+++ client/src/pages/ReservationCheck.tsx
@@ -1,23 +1,37 @@
 import { reservationCheckdummy } from "../dummy/reservationcheckdummy";
+import {
+  ResCheckContainer,
+  ResCheckTitle,
+  ResCheckCards,
+  ResImgContainer,
+  ImgResSzing,
+  ImgStyle,
+  ResInfoContainer,
+  StatusContainer,
+  ResInformation,
+  ResDate,
+  ResItemCount,
+  ResTotalPrice,
+  ResButtonsContainer,
+  ButtonStyle,
+  NoButtons,
+} from '../styles/MyPage/ReservationCheck';
 
 function ReservationCheck() {
   const { data } = reservationCheckdummy;
   
   return (
-    <div className="border-[1px] border-[#4771B7] px-[50px] py-16 space-y-5">
-      <p className="font-medium text-2xl pl-4">예약 내역 조회</p>
+    <ResCheckContainer>
+      <ResCheckTitle>예약 내역 조회</ResCheckTitle>
       {data.map((reservation) => (
-        <div 
-          key={reservation.storeId} 
-          className="border-[1.5px] border-[#4771B7] w-[700px] h-[200px] rounded-lg p-5 flex flex-row justify-center items-center space-x-12"
-        >
-          <div className="relative w-[146px] h-[127px]">
-            <div className="absolute inset-0 w-[146px] h-[127px]">
-              <img className="w-full h-full object-cover rounded-md" src={reservation.storeImage} alt="reservation image" />
-            </div>
-          </div>
-          <div className="flex flex-col w-[100%] space-y-2">
-            <div className="flex justify-end w-[100%]">
+        <ResCheckCards key={reservation.storeId}>
+          <ResImgContainer>
+            <ImgResSzing>
+              <ImgStyle src={reservation.storeImage} alt="reservation image" />
+            </ImgResSzing>
+          </ResImgContainer>
+          <ResInfoContainer>
+            <StatusContainer>
               <div 
                 className={
                   `
@@ -39,36 +53,36 @@ function ReservationCheck() {
               >
                 <span>{reservation.reservationStatus}</span>
               </div>
-            </div>
-            <div className="flex flex-col w-[100%] space-y-2">
+            </StatusContainer>
+            <ResInformation>
               <div>
                 <span className="text-[16px]">{reservation.reservationDate}</span>
               </div>
               <div className="space-x-3">
-                <span className="text-[20px] font-semibold">{reservation.storeName}</span>
-                <span className="text-[15px] text-[#868686] font-medium">총 {reservation.itemCount}개 상품 결제</span>
+                <ResDate>{reservation.storeName}</ResDate>
+                <ResItemCount>총 {reservation.itemCount}개 상품 결제</ResItemCount>
               </div>
               <div>
-                <span className="text-[15px] font-semibold">결제금액: {reservation.totalPrice}원</span>
+                <ResTotalPrice>결제금액: {reservation.totalPrice}원</ResTotalPrice>
               </div>
-            </div>
-            <div className="flex justify-end space-x-3 font-medium text-[15px]">
+            </ResInformation>
+            <ResButtonsContainer>
               {reservation.reservationStatus === "예약 완료" && (
                 <div>
-                  <button className="bg-[#F3F5F7] p-2 rounded-lg" type="button">예약 수정</button>
-                  <button className="bg-[#F3F5F7] p-2 rounded-lg" type="button">예약 삭제</button>
+                  <ButtonStyle type="button">예약 수정</ButtonStyle>
+                  <ButtonStyle type="button">예약 삭제</ButtonStyle>
                 </div>
               )}
               {(reservation.reservationStatus === "예약 취소" || reservation.reservationStatus === "이용 완료") && (
                 <div>
-                  <p className="h-[38.5px]"></p>
+                  <NoButtons></NoButtons>
                 </div>
               )}
-            </div>
-          </div>
-        </div>
+            </ResButtonsContainer>
+          </ResInfoContainer>
+        </ResCheckCards>
       ))}
-    </div>
+    </ResCheckContainer>
   );
 }
 
--- client/src/styles/MyPage/ReservationCheck.ts
+++ client/src/styles/MyPage/ReservationCheck.ts
@@ -1,1 +1,102 @@
 import tw from 'tailwind-styled-components';
+
+export const ResCheckContainer = tw.div`
+    border-[1px]
+    border-[#4771B7]
+    px-[50px]
+    py-16
+    space-y-5
+`;
+
+export const ResCheckTitle = tw.p`
+    font-medium
+    text-2xl
+    pl-4
+`;
+
+export const ResCheckCards = tw.div`
+    border-[1.5px]
+    border-[#4771B7]
+    w-[700px]
+    h-[200px]
+    rounded-lg
+    p-5
+    flex
+    flex-row
+    justify-center
+    items-center
+    space-x-12
+`;
+
+export const ResImgContainer = tw.div`
+    relative
+    w-[146px]
+    h-[127px]
+`;
+
+export const ImgResSzing = tw.div`
+    absolute
+    inset-0
+    w-[146px]
+    h-[127px]
+`;
+
+export const ImgStyle = tw.img`
+    w-full
+    h-full
+    object-cover
+    rounded-md
+`;
+
+export const ResInfoContainer = tw.div`
+    flex
+    flex-col
+    w-[100%]
+    space-y-2
+`;
+
+export const StatusContainer = tw.div`
+    flex
+    justify-end
+    w-[100%]
+`;
+
+export const ResInformation = tw.div`
+    flex
+    flex-col
+    w-[100%]
+    space-y-2
+`;
+
+export const ResDate = tw.span`
+    text-[20px]
+    font-semibold
+`;
+
+export const ResItemCount = tw.span`
+    text-[15px]
+    text-[#868686]
+    font-medium
+`;
+
+export const ResTotalPrice = tw.span`
+    text-[15px]
+    font-semibold
+`;
+
+export const ResButtonsContainer = tw.div`
+    flex
+    justify-end
+    space-x-3
+    font-medium
+    text-[15px]
+`;
+
+export const ButtonStyle = tw.button`
+    bg-[#F3F5F7]
+    p-2 rounded-lg
+`;
+
+export const NoButtons = tw.p`
+    h-[38.5px]
+`;
