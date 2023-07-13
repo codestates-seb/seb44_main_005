b9aefc240cbdcfce47dbcdc2eee8efdbab406291
chen4023 <ads4023@naver.com>
2023-07-13 15:45:15 +0900

    [feat] 검색 로딩창 구현

--- client/src/pages/CategoryPage.tsx
+++ client/src/pages/CategoryPage.tsx
@@ -1,5 +1,5 @@
 import { Link, useSearchParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import CategoryCard from '../components/Categorybar/CategoryCard';
 import {
@@ -11,48 +11,54 @@ import {
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { categoryData } from '../store/categoryAtom';
 import { search, searchKeyword } from '../store/searchbarAtom';
+import Loading from '../components/Loading/Loading';
 
 function CategoryPage() {
   const url = import.meta.env.VITE_APP_API_URL;
   const [searchParams] = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
   const categoryName = searchParams.get('category_name');
   const sort = searchParams.get('sort');
 
-  //전역상태변수
+  // 전역 상태 변수
   const keyword = useRecoilValue(searchKeyword);
   const [isSearch, setIsSearch] = useRecoilState(search);
   const [category, setCategory] = useRecoilState(categoryData);
-  // 어떤 데이터를 보여줄 것인가 ?
-  const isSearchResult = !!keyword;
 
   useEffect(() => {
     const fetchData = async () => {
       let data;
-      //검색조건이 있을 때
-      if (isSearchResult) {
+      // 검색 조건이 있을 때
+      if (keyword) {
         setIsSearch(true);
+        setIsLoading(true);
         const res = await fetch(`${url}/search?keyword=${keyword}`);
         data = await res.json();
         if (res.status !== 200) throw res;
       } else {
-        // 검색조건이 없을 때
+        // 검색 조건이 없을 때
         const res = await fetch(
           `${url}/stores?category=${categoryName}&sort_field=${sort}`
         );
-
         data = await res.json();
         setIsSearch(false);
-        //에러처리
+        // 에러 처리
         if (res.status !== 200) throw res;
       }
+      // 2초 동안 로딩 표시
+
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 500);
       setCategory(data);
     };
+
     fetchData();
   }, [categoryName, sort]);
 
   return (
     <Style>
-      <CategoryContainer>
+      <CategoryContainer style={{ display: isLoading ? 'none' : 'flex' }}>
         <span className="font-semibold text-2xl">
           전체상품 {category.pageInfo[0].storeCount}
         </span>
@@ -93,11 +99,15 @@ function CategoryPage() {
           )}
         </Option>
       </CategoryContainer>
-      <Category>
-        {category.data.map((el) => {
-          return <CategoryCard data={el} key={el.storeId} />;
-        })}
-      </Category>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Category>
+          {category.data.map((el) => (
+            <CategoryCard data={el} key={el.storeId} />
+          ))}
+        </Category>
+      )}
     </Style>
   );
 }
