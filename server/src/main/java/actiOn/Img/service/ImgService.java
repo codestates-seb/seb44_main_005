a3f4873b4a3b1851a6d73fc839acf904fef1de51
isu-nice <isu010614@naver.com>
2023-07-13 14:21:10 +0900

    [feat] 프로필 이미지 상태 변경 기능 구현 (#84)

--- server/src/main/java/actiOn/Img/service/ImgService.java
+++ server/src/main/java/actiOn/Img/service/ImgService.java
@@ -41,7 +41,7 @@ public class ImgService {
     }
 
     // 기본 프로필 이미지 경로 저장하는 메서드
-    public ProfileImg createDefaultProfileImg(Member member) {
+    public ProfileImg setDefaultProfileImg(Member member) {
         ProfileImg profileImg = new ProfileImg();
         profileImg.setLink("default Link"); // TODO 변경 해주어야 함
         profileImg.setMember(member);
@@ -49,53 +49,33 @@ public class ImgService {
         return profileImg;
     }
 
+    // 기존 프로필 이미지 status DELETED로 변경
+    public ProfileImg updateCurrentProfileImageStatus(Member member) {
+        ProfileImg currentProfileImg = findProfileImgByMember(member);
+        currentProfileImg.setImgStatus(ProfileImg.ProfileImgStatus.PROFILE_DELETED);
 
-    public static String generateRandomName(){
-        String uuid = UUID.randomUUID().toString();
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 25;
-        Random random = new Random();
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-        return uuid+generatedString;
+        return profileImgRepository.save(currentProfileImg);
     }
 
-    public String uploadImage(MultipartFile file, String imageName) throws IOException {
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion(REGION)
-                .build();
+    // 프로필 이미지 등록
+    public ProfileImg uploadProfileImage(MultipartFile file, Member member) throws IOException {
+        String imageName = generateRandomName();
+        String fileUrl = uploadImage(file, imageName);
 
-        s3Client.putObject(new PutObjectRequest(BUCKET_NAME, imageName, file.getInputStream(), null));
-
-        String fileUrl = s3Client.getUrl(BUCKET_NAME, imageName).toString();
-        return fileUrl;
-    }
-
-    public void uploadProfileImage(MultipartFile file, Member member) {
-        try {
-            String imageName = "test";
-            String fileUrl = uploadImage(file, imageName);
-
-            ProfileImg profileImg = new ProfileImg();
-            profileImg.setLink(fileUrl);
-            profileImg.setMember(member);
-            profileImgRepository.save(profileImg);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        ProfileImg profileImg = new ProfileImg();
+        profileImg.setLink(fileUrl);
+        profileImg.setMember(member);
+        return profileImgRepository.save(profileImg);
     }
 
     public List<StoreImg> uploadStoreImage(List<MultipartFile> files, long storeId) {
         String randomString = generateRandomName();
         try {
-            int index=1;
+            int index = 1;
             List<StoreImg> storeImgs = new ArrayList<>();
             for (MultipartFile file : files) {
 
-                String imageName = String.valueOf(storeId) +  randomString + String.valueOf(index);
+                String imageName = String.valueOf(storeId) + randomString + String.valueOf(index);
                 StoreImg storeImg = new StoreImg();
                 Store findStore = storeRepository.findById(storeId).orElseThrow(() -> new BusinessLogicException(ExceptionCode.STORE_NOT_FOUND));
                 if (findStore == null) {
@@ -103,7 +83,7 @@ public class ImgService {
                 }
                 String fileUrl = uploadImage(file, imageName);
                 storeImg.setLink(fileUrl);
-                if (file.equals(files.get(0))){
+                if (file.equals(files.get(0))) {
                     storeImg.setIsThumbnail(true);
                 }
                 storeImgs.add(storeImgRepository.save(storeImg));
@@ -117,19 +97,46 @@ public class ImgService {
         }
     }
 
-    public Optional<ProfileImg> findProfileImgByMember(Member member) {
-        return profileImgRepository.findByMember(member);
+    private ProfileImg findProfileImgByMember(Member member) {
+        Optional<ProfileImg> profileImg = profileImgRepository.findByMember(member);
+
+        if (profileImg.isEmpty()) {
+            throw new BusinessLogicException(ExceptionCode.PROFILE_IMAGE_NOT_FOUND);
+        }
+        return profileImg.get();
     }
 
     public Optional<StoreImg> findStoreImgByStore(Store store) {
         return storeImgRepository.findByStore(store);
     }
 
-    public void deleteProfileImg(Member member) {
-        //Todo 진짜 삭제하지 않고 기본 프로필로 바꾸는 작업
-    }
-
     public void deleteStoreImg(Store store) {  // 스토어 이미지들 삭제
 
+    }
+
+    private String uploadImage(MultipartFile file, String imageName) throws IOException {
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(REGION)
+                .build();
+
+        s3Client.putObject(new PutObjectRequest(
+                BUCKET_NAME, imageName, file.getInputStream(), null)
+        );
+
+        String fileUrl = s3Client.getUrl(BUCKET_NAME, imageName).toString();
+        return fileUrl;
+    }
+
+    private String generateRandomName() {
+        String uuid = UUID.randomUUID().toString();
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 25;
+        Random random = new Random();
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        return uuid + generatedString;
     }
 }
