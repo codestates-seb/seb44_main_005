c5dc291dad0b6737617b7acc53dfb485e1e5f6ef
kminvita <vita_mink@daum.net>
2023-07-13 14:01:19 +0900

    [design] 판매 서비스 관리 UI 구현 완료

--- client/src/pages/StoreCheck.tsx
+++ client/src/pages/StoreCheck.tsx
@@ -1,28 +1,48 @@
+import { storecheckdummy } from "../dummy/storecheckdummy";
+import { 
+  StoreButtonStyle,
+  ButtonsContainer,
+  ImgContainer,
+  ImgInnerContainer,
+  ImgStyle,
+  StoreCards,
+  StoreCheckContainer,
+  StoreCheckTitle, 
+  StoreInfoContainer,
+  StoreName
+} from "../styles/MyPage/StoreCheck";
+
 function StoreCheck() {
+  const { stores } = storecheckdummy;
+  
   const handleDeleteClick = () => {
     alert('정말 삭제하시겠습니가?');
   };
 
   return (
-    <div className="border-[1px] border-[#4771B7] px-[50px] py-16 space-y-5">
-      <span className="font-medium text-2xl pl-4">판매 서비스 관리</span>
-      <div className="border-[1.5px] border-[#4771B7] w-[700px] h-[200px] rounded-lg p-5 flex flex-row justify-center items-center space-x-12">
-        <div className="relative w-[146px] h-[127px]">
-          <div className="absolute inset-0 w-[146px] h-[127px]">
-            <img className="w-full h-full object-cover rounded-md" src="https://i.pinimg.com/564x/e9/73/64/e97364480b1511dbe7739a1d1b6f6762.jpg" alt="StoreCheck image" />
-          </div>
-        </div>
-        <div className="flex flex-col w-[100%] space-y-20">
-          <div className="pt-5">
-            <span className="text-[20px] font-semibold">언더워터플레이 함덕점</span>
-          </div>
-          <div className="flex justify-end space-x-3 font-medium text-[15px]">
-            <button className="bg-[#F3F5F7] p-2 rounded-lg" type="button">업체 수정</button>
-            <button className="bg-[#F3F5F7] p-2 rounded-lg" type="button" onClick={handleDeleteClick}>업체 삭제</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <StoreCheckContainer>
+      <StoreCheckTitle>판매 서비스 관리</StoreCheckTitle>
+      {stores.map((store) => (
+        <StoreCards key={store.storeId}>
+          <ImgContainer>
+            <ImgInnerContainer>
+              <ImgStyle src={store.storeImage} alt="store image" />
+            </ImgInnerContainer>
+          </ImgContainer>
+          <StoreInfoContainer>
+            <div className="pt-5">
+              <StoreName>{store.storeName}</StoreName>
+            </div>
+            <ButtonsContainer>
+              <StoreButtonStyle type="button">업체 수정</StoreButtonStyle>
+              <StoreButtonStyle type="button" onClick={handleDeleteClick}>
+                업체 삭제
+              </StoreButtonStyle>
+            </ButtonsContainer>
+          </StoreInfoContainer>
+        </StoreCards>
+      ))}
+    </StoreCheckContainer>
   );
 }
 
