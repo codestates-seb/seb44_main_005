64d65607c47c4ea2de03f2e6aee9d6336a113025
kminvita <vita_mink@daum.net>
2023-07-14 09:53:39 +0900

    [design] 스타일 분리 #19

--- client/src/pages/Partner.tsx
+++ client/src/pages/Partner.tsx
@@ -1,6 +1,29 @@
 import { useEffect, useState } from 'react';
 import SelectBox from '../components/Partner/SelectBox';
 
+import {
+  RegiContainer,
+  PartnerContainer,
+  RegiTitle,
+  FormContainer,
+  RepreNameContainer,
+  RegiNumberContainer,
+  RegiNumberNoWrite,
+  RegiNumberCorrect,
+  RegiNumberWrong,
+  CompanyName,
+  CommonInput,
+  RegiNumberInput,
+  BusinessInput,
+  BusinessContanier,
+  SectorContainer,
+  BusinessSectorContainer,
+  FormRegiButton,
+  FormRegiContainer,
+  LabelStyle,
+  OpeningContainer
+} from '../styles/Partner/Partner';
+
 function Partner() {
   const [regiNumber, setRegiNumber] = useState('');
   const [isInputTouched, setIsInputTouched] = useState(false);
@@ -14,8 +37,8 @@ function Partner() {
     setRegiNumber(formattedRegiNumber);
   };
 
-  const isRegiNumberValid = regiNumber.length === 12;
-  const isRegiNumberIncomplete = regiNumber.length > 0 && regiNumber.length <12;
+  const isRegiNumberValid = regiNumber.match(/^\d{3}-\d{2}-\d{5}$/);
+  const isRegiNumberIncomplete = regiNumber.length > 0 && !isRegiNumberValid;
 
   useEffect(() => {
     setIsInputTouched(true);
@@ -39,27 +62,25 @@ function Partner() {
   };
 
   return (
-    <div className="flex justify-center items-center h-[80%]">
+    <PartnerContainer>
       <div className="p-10">
-        <div className="flex flex-col justify-center w-[700px]">
-          <p className="text-2xl font-semibold justify-start p-10">
+        <RegiContainer>
+          <RegiTitle>
             파트너 등록하기
-          </p>
-          <form className="justify-center">
-            <div className="flex space-x-5 justify-end py-2">
-              <label className="">대표자 명</label>
-              <input
-                className="border-[1px] border-[#9A9A9A] rounded-md w-[78%] px-2 py-[2px]"
+          </RegiTitle>
+          <FormContainer>
+            <RepreNameContainer>
+              <label>대표자 명</label>
+              <CommonInput
                 placeholder="ex. 홍길동"
                 type="text"
               />
-            </div>
-            <div className="flex space-x-4 justify-end py-2">
+            </RepreNameContainer>
+            <RegiNumberContainer>
               <label>사업자등록번호</label>
               <div className="w-[78%] flex flex-row">
                 <div className="pr-4 w-[100%]">
-                  <input
-                    className="border-[1px] border-[#9A9A9A] rounded-md w-[100%]  px-2 py-[2px]"
+                  <RegiNumberInput
                     placeholder="ex. 123-45-67890"
                     type="text"
                     value={regiNumber}
@@ -68,15 +89,13 @@ function Partner() {
                     onBlur={() => setIsInputTouched(true)}
                   />
                   {isInputTouched && !regiNumber && (
-                    <p className="pt-1 text-gray-950">숫자만 입력해주세요.</p>
+                    <RegiNumberNoWrite>숫자만 입력해주세요.</RegiNumberNoWrite>
                   )}
                   {isRegiNumberValid && (
-                    <p className="pt-1 text-green-500">올바른 입력입니다.</p>
+                    <RegiNumberCorrect>올바른 입력입니다.</RegiNumberCorrect>
                   )}
                   {isRegiNumberIncomplete && (
-                    <p className="pt-1 text-red-500">
-                      사업자 등록번호는 10자리로 입력되어야 합니다.
-                    </p>
+                    <RegiNumberWrong>사업자 등록번호는 10자리로 입력되어야 합니다.</RegiNumberWrong>
                   )}
                 </div>
                 <button
@@ -90,28 +109,25 @@ function Partner() {
                   중복확인
                 </button>
               </div>
-            </div>
-            <div className="flex space-x-5 justify-end py-2">
-              <label className="">업체명</label>
-              <input
-                className="border-[1px] border-[#9A9A9A] rounded-md w-[78%]  px-2 py-[2px]"
+            </RegiNumberContainer>
+            <CompanyName>
+              <label>업체명</label>
+              <CommonInput
                 placeholder="ex. OO레저"
                 type="text"
               />
-            </div>
-            <div className="flex space-x-5 justify-end py-2">
-              <label className="">개업 일자</label>
-              <input
-                className="border-[1px] border-[#9A9A9A] rounded-md  w-[78%] px-2 py-[2px]"
+            </CompanyName>
+            <OpeningContainer>
+              <label>개업 일자</label>
+              <CommonInput
                 placeholder="2023-00-00"
                 type="date"
               />
-            </div>
-            <div className="flex flex-row justify-end py-2">
-              <div className="flex space-x-5 justify-end w-[55%]">
-                <label className="w-[9%]">업태</label>
-                <input
-                  className="border-[1px] border-[#9A9A9A] rounded-md w-[60%]  px-2 py-[2px]"
+            </OpeningContainer>
+            <BusinessSectorContainer>
+              <BusinessContanier>
+                <LabelStyle>업태</LabelStyle>
+                <BusinessInput
                   type="text"
                   name="business"
                   value="스포츠 및 여가관련 서비스업"
@@ -119,26 +135,25 @@ function Partner() {
                   onClick={(e) => e.preventDefault()}
                   style={{ pointerEvents: 'none' }}
                 />
-              </div>
-              <div className="flex space-x-5 justify-end w-[45%]">
-                <label className="">업종</label>
+              </BusinessContanier>
+              <SectorContainer>
+                <label>업종</label>
                 <div>
                   <SelectBox />
                 </div>
-              </div>
-            </div>
-            <div className="flex justify-center pt-4">
-              <button
-                className="font-semibold rounded-md text-xl px-14 py-3 bg-[#4771B7] text-white"
+              </SectorContainer>
+            </BusinessSectorContainer>
+            <FormRegiContainer>
+              <FormRegiButton
                 type="submit"
               >
                 등록하기
-              </button>
-            </div>
-          </form>
-        </div>
+              </FormRegiButton>
+            </FormRegiContainer>
+          </FormContainer>
+        </RegiContainer>
       </div>
-    </div>
+    </PartnerContainer>
   );
 }
 
