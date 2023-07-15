d3d23f0bd54fbd2871088c5291a040ab4e78841f
isu-nice <isu010614@naver.com>
2023-07-16 00:10:46 +0900

    [feat] 아이템 조회 기능 추가 (#96)

--- server/src/main/java/actiOn/item/repository/ItemRepository.java
+++ server/src/main/java/actiOn/item/repository/ItemRepository.java
@@ -3,5 +3,8 @@ package actiOn.item.repository;
 import actiOn.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Optional<Item> findItemByItemId(Long itemId);
 }
