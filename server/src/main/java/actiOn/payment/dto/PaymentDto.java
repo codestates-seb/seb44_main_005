68f9b40575574ecd0468cc63d4cb678a2f9cffef
isu-nice <isu010614@naver.com>
2023-07-15 13:08:08 +0900

    [feat] 결제 dto (#98)

--- server/src/main/java/actiOn/payment/dto/PaymentDto.java
+++ server/src/main/java/actiOn/payment/dto/PaymentDto.java
@@ -1,4 +1,28 @@
 package actiOn.payment.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+
+@Builder
+@Getter
 public class PaymentDto {
+    @NotBlank
+    private String paymentKey;
+
+    @NotBlank
+    private String payType;
+
+    @NotBlank
+    private String payMethod;
+
+    @NotBlank
+    @Range(min = 0, max = 100_000_000)
+    private Long totalAmount;
+
+    @NotBlank
+    private String orderName;
+
 }
