e807b8387b73b2a7e0a3e8deafe454eff29879e4
isu-nice <isu010614@naver.com>
2023-07-15 13:08:33 +0900

    [feat] 결제 repository 구성 (#98)

--- server/src/main/java/actiOn/payment/repository/PaymentRepository.java
+++ server/src/main/java/actiOn/payment/repository/PaymentRepository.java
@@ -1,4 +1,12 @@
 package actiOn.payment.repository;
 
-public interface PaymentRepository {
+import actiOn.payment.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(String orderId);
+
+    Optional<Payment> findByPaymentKeyAndCustomer_Email(String paymentKey, String email);
 }
