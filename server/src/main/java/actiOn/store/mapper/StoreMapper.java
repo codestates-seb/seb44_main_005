2cfdd3544349a883545527ffa7ce728656b35d1b
isu-nice <isu010614@naver.com>
2023-07-16 00:12:00 +0900

    [chore] 컨벤션 수정 (#96)

--- server/src/main/java/actiOn/store/mapper/StoreMapper.java
+++ server/src/main/java/actiOn/store/mapper/StoreMapper.java
@@ -2,9 +2,9 @@ package actiOn.store.mapper;
 
 
 import actiOn.item.entity.Item;
+import actiOn.store.dto.StoreIdResponseDto;
 import actiOn.store.dto.StorePatchDto;
 import actiOn.store.dto.StorePostDto;
-import actiOn.store.dto.StoreIdResponseDto;
 import actiOn.store.dto.StoreResponseDto;
 import actiOn.store.entity.Store;
 import org.mapstruct.Mapper;
@@ -14,8 +14,12 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface StoreMapper {
     Store storePostDtoToStore(StorePostDto storePostDto);
+
     Store storePatchDtoToStore(StorePatchDto storePatchDto);
+
     StoreResponseDto storeToStoreResponseDto(Store store);
+
     StoreIdResponseDto storeToStorePostResponseDto(Store store);
+
     List<Item> storePostDtoItemsToItemEntities(List<Item> itemList);
 }
