06034c81a678f1470fcef7f3a3e0e92bf831baa1
kminvita <vita_mink@daum.net>
2023-07-18 01:23:32 +0900

    [feat] GET요청으로 판매 상품 불러오기 기능 구현

--- client/src/pages/StoreCheck.tsx
+++ client/src/pages/StoreCheck.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { storecheckdummy } from '../dummy/storecheckdummy';
 import {
   StoreButtonStyle,
@@ -13,7 +14,35 @@ import {
 } from '../styles/MyPage/StoreCheck';
 
 function StoreCheck() {
-  const { stores } = storecheckdummy;
+  const APIURL = import.meta.env.VITE_APP_API_URL;
+  const [storesData, setStoresData] = useState([]);
+
+  // const { stores } = storecheckdummy;
+
+  useEffect(() => {
+    fetchStores();
+  },[]);
+
+  const fetchStores = async () => {
+    try {
+      const ACCESS_TOKEN = sessionStorage.getItem('Authorization');
+      const res = await fetch(`${APIURL}/mystores`, {
+        method: 'GET',
+        headers: {
+          'Authorization': ACCESS_TOKEN
+        }
+      });
+
+      if(res.ok) {
+        const data = await res.json();
+        setStoresData(data.stores);
+      } else {
+        console.error('데이터가 없습니다.', res.status);
+      }
+    } catch (error) {
+      console.error('에러가 발생했습니다.', error);
+    }
+  };
 
   const handleDeleteClick = () => {
     alert('정말 삭제하시겠습니가?');
@@ -22,7 +51,7 @@ function StoreCheck() {
   return (
     <StoreCheckContainer>
       <StoreCheckTitle>판매 서비스 관리</StoreCheckTitle>
-      {stores.map((store) => (
+      {storesData.map((store) => (
         <StoreCards key={store.storeId}>
           <ImgContainer>
             <ImgInnerContainer>
