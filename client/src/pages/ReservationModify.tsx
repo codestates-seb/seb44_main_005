27988d81d0f6c1b850eba5e9ad4a8c640b39a01b
chen4023 <ads4023@naver.com>
2023-07-19 13:19:09 +0900

    [feat] 예약 수정페이지 기능구현

--- client/src/pages/ReservationModify.tsx
+++ client/src/pages/ReservationModify.tsx
@@ -1,4 +1,5 @@
-import { reservation } from '../dummy/reservation';
+import { useEffect, useState } from 'react';
+// import { reservation } from '../dummy/reservation';
 import {
   ReservationContainer,
   ReservationTitle,
@@ -12,59 +13,148 @@ import {
   AmountBox,
   RuleBox,
   PaymentButton,
+  InputContainer,
+  ModifyButton,
 } from '../styles/Reservation/ReservationModify';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+type CProps = {
+  storeName: string;
+  reservationDate: string;
+  totalPrice: number;
+  reservationName: string;
+  reservationPhone: string;
+  reservationEmail: string;
+  reservationItems: any[];
+};
 
 function ReservationModify() {
+  const url = import.meta.env.VITE_APP_API_URL;
+  const initialReservation = {
+    storeName: '',
+    reservationDate: '',
+    totalPrice: 0,
+    reservationName: '',
+    reservationPhone: '',
+    reservationEmail: '',
+    reservationItems: [],
+  };
+
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const reservationId = searchParams.get('reservationId');
+
+  const [data, setData] = useState<CProps>(initialReservation);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+
+  const HandlerName = (e) => {
+    setName(e.currentTarget.value);
+  };
+  const HandlerPhone = (e) => {
+    setPhone(e.currentTarget.value);
+  };
+  const HandlerEmail = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  //예약자 정보변경
+  const handleModify = () => {
+    try {
+      fetch(`${url}/reservations/${reservationId}`, {
+        method: 'PATCH',
+        headers: { Authorization: sessionStorage.getItem('Authorization') },
+        body: JSON.stringify({
+          reservationName: name,
+          reservationPhone: phone,
+          reservationEmail: email,
+        }),
+      });
+      alert('수정이 완료 되었습니다');
+      location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //예약 취소
+  const handleCancel = () => {
+    try {
+      fetch(`${url}/reservations/${reservationId}`, {
+        method: 'DELETE',
+        headers: { Authorization: sessionStorage.getItem('Authorization') },
+      });
+      navigate('/my/order'); // 예약내역 조회 페이지로 이동
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //Get
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(`${url}/reservations/${reservationId}`, {
+        headers: { Authorization: sessionStorage.getItem('Authorization') },
+      })
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    };
+    fetchData();
+  }, [reservationId]);
   return (
     <div className="flex">
       <ReservationContainer>
-        <ReservationTitle>{reservation.storeName}</ReservationTitle>
+        <ReservationTitle>{data.storeName}</ReservationTitle>
         <Title>예약정보</Title>
-        <div className="ml-3">
-          <SubTitle>1. 결제한 상품</SubTitle>
-          {reservation.reservationItems.map((el) => {
-            return (
-              <div className="mb-3" key={el.item}>
-                {el.item} x <span>{el.ticketCount}</span>
-              </div>
-            );
-          })}
-          <SubTitle>2. 예약 날짜</SubTitle>
-          {reservation.reservationDate}
-          <ReservationInfo>
-            <Title>예약자 정보</Title>
-            <button className="relative left-[520px] bottom-[50px] bg-[#F3F5F7] rounded-lg font-semibold text-[14px] p-[10px]">
-              수정하기
-            </button>
-            <div className="flex ml-7 mb-3 items-center">
-              <InfoTitle>예약자</InfoTitle>
-              <div className="relative">
-                <ReservationInput
-                  type="text"
-                  placeholder={reservation.reservationName}
-                />
-                <InputRequire>필수</InputRequire>
-              </div>
-            </div>
-            <div className="flex ml-7 mb-3 items-center">
-              <InfoTitle>연락처</InfoTitle>
-              <div className="relative">
-                <ReservationInput
-                  type="text"
-                  placeholder={reservation.reservationPhone}
-                />
-                <InputRequire>필수</InputRequire>
-              </div>
-            </div>
-            <div className="flex ml-7 mb-3 items-center">
-              <InfoTitle>이메일</InfoTitle>
+        <SubTitle>1. 결제한 상품</SubTitle>
+        {data.reservationItems.map((el) => {
+          return (
+            <li className="mb-3 list-none" key={el.item}>
+              {el.item} x <span>{el.ticketCount}</span>
+            </li>
+          );
+        })}
+        <SubTitle>2. 예약 날짜</SubTitle>
+        {data.reservationDate}
+        <ReservationInfo>
+          <Title>예약자 정보</Title>
+          <ModifyButton onClick={handleModify}>수정하기</ModifyButton>
+          <InputContainer>
+            <InfoTitle>예약자</InfoTitle>
+            <div className="relative">
               <ReservationInput
                 type="text"
-                placeholder={reservation.reservationEmail}
+                value={name}
+                onChange={HandlerName}
+                placeholder={data.reservationName}
               />
+              <InputRequire>필수</InputRequire>
             </div>
-          </ReservationInfo>
-        </div>
+          </InputContainer>
+          <InputContainer>
+            <InfoTitle>연락처</InfoTitle>
+            <div className="relative">
+              <ReservationInput
+                type="tel"
+                value={phone}
+                onChange={HandlerPhone}
+                maxLength={13}
+                placeholder={data.reservationPhone}
+              />
+              <InputRequire>필수</InputRequire>
+            </div>
+          </InputContainer>
+          <InputContainer>
+            <InfoTitle>이메일</InfoTitle>
+            <ReservationInput
+              type="email"
+              value={email}
+              onChange={HandlerEmail}
+              placeholder={data.reservationEmail}
+            />
+          </InputContainer>
+        </ReservationInfo>
       </ReservationContainer>
       <PaymentInfoBox>
         <div className="m-5 pb-3 text-lg border-b-[1px] border-black">
@@ -72,11 +162,11 @@ function ReservationModify() {
         </div>
         <div className="m-7 flex justify-between">
           <div>주문금액</div>
-          <div>{reservation.totalPrice.toLocaleString('ko-KR')} 원</div>
+          <div>{data.totalPrice.toLocaleString('ko-KR')} 원</div>
         </div>
         <AmountBox>
           <div>총 결제금액</div>
-          <div>{reservation.totalPrice.toLocaleString('ko-KR')} 원</div>
+          <div>{data.totalPrice.toLocaleString('ko-KR')} 원</div>
         </AmountBox>
         <RuleBox>
           <div className="font-semibold">예약취소 규정</div>
@@ -86,7 +176,9 @@ function ReservationModify() {
             <li>부분 사용 및 부분 취소는 불가능합니다.</li>
           </ul>
         </RuleBox>
-        <PaymentButton type="button">예약취소</PaymentButton>
+        <PaymentButton type="button" onClick={handleCancel}>
+          예약취소
+        </PaymentButton>
       </PaymentInfoBox>
     </div>
   );
