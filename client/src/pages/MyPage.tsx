badbf4fbd94823f1ff343edbde90299eedb502fc
kminvita <vita_mink@daum.net>
2023-07-20 01:50:38 +0900

    [rename] 컴포넌트 분리 작업

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 import Modal from '../components/MyPage/Modal';
+import BusinessSpaceSection from '../components/MyPage/BusinessSpaceSection';
 import defaultProfileImg from '../assets/profile.svg';
 
 import {
-  BusinessCategory,
-  BusinessSpace,
   ButtonGrid,
   ButtonStyle,
   ImgStyle,
@@ -12,13 +11,11 @@ import {
   MySpace,
   NicknameAccent,
   TopSpace,
-  UserInfo,
-  UserInfoTitle,
-  BusinessCategoryTitle,
   MyPageContainer,
   MyBioContainer,
   PhotoInputStyle,
 } from '../styles/MyPage/MyPage';
+import UserInfoSection from '../components/MyPage/UserInfoSection';
 
 function MyPage() {
   const APIURL = import.meta.env.VITE_APP_API_URL;
@@ -26,11 +23,9 @@ function MyPage() {
 
   const [showBusinessSpace, setShowBusinessSpace] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
   const [userData, setUserData] = useState(null);
   const [partnerData, setPartnerData] = useState(null);
-  const [accessDenied, setAccessDenied] = useState(false);
-  const [isDeletingPhoto, setIsDeletingPhoto] = useState(false);
+  const [_, setAccessDenied] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -118,80 +113,6 @@ function MyPage() {
     setShowModal(false);
   };
 
-  const handlePhotoChange = async (e) => {
-    const file = e.target.files[0];
-    setSelectedPhoto(file);
-
-    try  {
-      const ACCESS_TOKEN = sessionStorage.getItem('Authorization');
-      const formData = new FormData();
-      formData.append('image', file);
-
-      const res = await fetch(`${APIURL}/mypage/profile`, {
-        method: 'PUT',
-        headers: {
-          'Authorization': ACCESS_TOKEN,
-        },
-        body: formData,
-      });
-
-      if (res.ok) {
-        console.log('프로필 업데이트 완료');
-        const imageUrl = URL.createObjectURL(file);
-        setSelectedPhoto(file);
-        sessionStorage.setItem('selectedPhoto', JSON.stringify(imageUrl));
-        fetchData(); // Fetch updated user data after profile update
-      } else {
-        console.error('프로필 업데이트 실패', res.status);
-      }
-    } catch (error) {
-      console.error('프로필 업데이트 에러', error);
-    }
-  };
-
-  const getProfileImage = () => {
-    if (profileImg === 'default image') {
-      return defaultProfileImg;
-    } else if (profileImg) {
-      return profileImg;
-    } else {
-      return defaultProfileImg;
-    }
-  };
-
-  const handlePhotoRemove = async () => {
-    setIsDeletingPhoto(true);
-
-    try {
-      const ACCESS_TOKEN = sessionStorage.getItem('Authorization');
-      const res = await fetch(`${APIURL}/mypage/profile`, {
-        method: 'DELETE',
-        headers: {
-          'Authorization': ACCESS_TOKEN,
-        },
-      });
-
-      if (res.ok) {
-        console.log('프로필 사진 삭제 완료');
-        setSelectedPhoto(null);
-        const input = document.getElementById('photoInput') as HTMLInputElement;
-        if (input) {
-          input.value = '';
-        }
-        setUserData((prevUserData) => ({
-          ...prevUserData,
-          profileImg: 'default image',
-        }));
-      } else {
-        console.error('프로필 사진 삭제 실패', res.status);
-      }
-    } catch (error) {
-      console.error('프로필 사진 삭제 에러', error);
-    } finally {
-      setIsDeletingPhoto(false);
-    }
-  };
-
   const handleEditComplete = (updatedUserData) => {
     setUserData({
       ...updatedUserData,
@@ -199,6 +120,8 @@ function MyPage() {
     });
     setShowModal(false);
   };
+
+  const profileImage = profileImg !== 'default image' ? profileImg : defaultProfileImg;
 
   return (
     <>
@@ -210,7 +133,7 @@ function MyPage() {
             </ButtonGrid>
             <TopSpace>
               <ImgStyle 
-                src={selectedPhoto ? URL.createObjectURL(selectedPhoto) : getProfileImage()}
+                src={profileImage}
                 alt="profile img" 
               />
               <MiniButtonGrid>
@@ -220,7 +143,6 @@ function MyPage() {
                     type='file'
                     accept='image/*'
                     style={{ display: 'none' }}
-                    onChange={handlePhotoChange} 
                   />
                   <PhotoInputStyle>사진 변경</PhotoInputStyle>
                 </label>
@@ -229,53 +151,20 @@ function MyPage() {
                     id='photoRemoveInput'
                     type='button'
                     style={{ display: 'none' }}
-                    onClick={handlePhotoRemove}
                   />
                   <PhotoInputStyle>사진 삭제</PhotoInputStyle>
                 </label>
               </MiniButtonGrid>
               <NicknameAccent>{nickname}</NicknameAccent>
             </TopSpace>
-            <MySpace>
-              <UserInfo>
-                <UserInfoTitle>닉네임</UserInfoTitle>
-                <span>{nickname}</span>
-              </UserInfo>
-              <UserInfo>
-                <UserInfoTitle>이메일</UserInfoTitle>
-                <span>{email}</span>
-              </UserInfo>
-              <UserInfo>
-                <UserInfoTitle>연락처</UserInfoTitle>
-                <span>{phoneNumber}</span>
-              </UserInfo>
-            </MySpace>
+            <UserInfoSection nickname={nickname} email={email} phoneNumber={phoneNumber} />
           </MySpace>
           <MySpace>
             <ButtonGrid>
               <ButtonStyle type="button" onClick={handleButtonClick}>등록한 업체보기</ButtonStyle>
             </ButtonGrid>
             {showBusinessSpace && (
-              <BusinessSpace>
-                <BusinessCategory>
-                  <BusinessCategoryTitle>업태</BusinessCategoryTitle>
-                  {partnerData.stores.map((_, index) => (
-                    <span key={index}>{businessRegi}</span>
-                  ))}
-                </BusinessCategory>
-                <BusinessCategory>
-                  <BusinessCategoryTitle>업종</BusinessCategoryTitle>
-                  {partnerData.stores.map((store, index) => (
-                    <span key={index}>{store.category}</span>
-                  ))}
-                </BusinessCategory>
-                <BusinessCategory>
-                  <BusinessCategoryTitle>업체명</BusinessCategoryTitle>
-                  {partnerData.stores.map((store, index) => (
-                    <span key={index}>{store.storeName}</span>
-                  ))}
-                </BusinessCategory>
-              </BusinessSpace>
+              <BusinessSpaceSection partnerData={partnerData} businessRegi={businessRegi} />
             )}
           </MySpace>
         </MyBioContainer>
