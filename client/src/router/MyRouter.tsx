3db9353910858001fb632a2ca88165d008c5e42b
kminvita <vita_mink@daum.net>
2023-07-21 01:44:02 +0900

    [feat] 미로그인시 마이페이지 관련 페이지 접근 불가 및 로그인페이지로 이동 기능 구현

--- client/src/router/MyRouter.tsx
+++ client/src/router/MyRouter.tsx
@@ -1,4 +1,6 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { isLoginState } from '../store/userInfoAtom';
 
 import MyPage from '../pages/MyPage';
 import ReservationModify from '../pages/ReservationModify';
@@ -11,10 +13,17 @@ import FooterLayout from '../components/Layout/FooterLayout';
 import SideBarLayout from '../components/Layout/SideBarLayout';
 
 function MyRouter() {
+  const isLoggedIn = useRecoilValue(isLoginState);
+
+  if(!isLoggedIn) {
+    return <Navigate to='/login' />
+  }
+
   return (
     <Routes>
       <Route element={<HeaderLayout />}>
         <Route element={<FooterLayout />}>
+          <Route path="/my/*" element={<Navigate to="/login" />} />
           <Route element={<SideBarLayout />}>
             <Route path="/my" element={<MyPage />} />
             <Route path="/my/wish" element={<WishList />} />
@@ -22,6 +31,7 @@ function MyRouter() {
             <Route path="/my/stores" element={<StoreCheck />} />
           </Route>
           <Route path="/my/order/edit" element={<ReservationModify />} />
+          <Route path="/store/*" element={<Navigate to="/login" />} />
           <Route path="/store/add" element={<StoreAdd />} />
           <Route path="/store/edit" element={<StoreAdd />} />
         </Route>
