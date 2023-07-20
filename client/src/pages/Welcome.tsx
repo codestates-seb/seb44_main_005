04f3f30d947f1a5e91ae3ab974336fcd25e7c810
chen4023 <ads4023@naver.com>
2023-07-20 23:12:46 +0900

    [feat] 웰컴페이지 인트로 구현&디자인 완료

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -2,6 +2,8 @@ import tw from 'tailwind-styled-components';
 import Page1 from '../components/IntroPage/Page1';
 import WelcomeLayout from '../components/Layout/WelcomeLayout';
 import Page2 from '../components/IntroPage/Page2';
+import Page3 from '../components/IntroPage/Page3';
+import Page4 from '../components/IntroPage/Page4';
 
 const Welcome = () => {
   return (
@@ -10,6 +12,8 @@ const Welcome = () => {
         <WelcomeContainer>
           <Page1 />
           <Page2 />
+          <Page3 />
+          <Page4 />
         </WelcomeContainer>
       }
     />
