36d9001cb53b2133f4ec0d24a5c14ea0ea944312
kminvita <vita_mink@daum.net>
2023-07-21 03:43:46 +0900

    [comment] 주석 삭제

--- client/src/router/MyRouter.tsx
+++ client/src/router/MyRouter.tsx
@@ -12,7 +12,6 @@ import HeaderLayout from '../components/Layout/HeaderLayout';
 import FooterLayout from '../components/Layout/FooterLayout';
 import SideBarLayout from '../components/Layout/SideBarLayout';
 
-// Function to check if user is logged in
 const requireLogin = (component) => {
   const isLoggedIn = useRecoilValue(isLoginState);
 
@@ -29,7 +28,6 @@ function MyRouter() {
       <Route element={<HeaderLayout />}>
         <Route element={<FooterLayout />}>
           <Route element={<SideBarLayout />}>
-            {/* Use the requireLogin function to conditionally render the components */}
             <Route path="/my" element={requireLogin(<MyPage />)} />
             <Route path="/my/wish" element={requireLogin(<WishList />)} />
             <Route path="/my/order" element={requireLogin(<ReservationCheck />)} />
