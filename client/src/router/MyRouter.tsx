0c6c6e05f2b9d0cb49396f0fd7fb4e7a7d978acf
kminvita <vita_mink@daum.net>
2023-07-23 02:10:02 +0900

    [feat] 마이라우터 수정

--- client/src/router/MyRouter.tsx
+++ client/src/router/MyRouter.tsx
@@ -1,7 +1,4 @@
-import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useRecoilValue } from 'recoil';
-import { isLoginState } from '../store/userInfoAtom';
+import { Route, Routes } from 'react-router-dom';
 
 import MyPage from '../pages/MyPage';
 import ReservationModify from '../pages/ReservationModify';
@@ -13,55 +10,20 @@ import HeaderLayout from '../components/Layout/HeaderLayout';
 import FooterLayout from '../components/Layout/FooterLayout';
 import SideBarLayout from '../components/Layout/SideBarLayout';
 
-const requireLogin = (component) => {
-  const isLoggedIn = useRecoilValue(isLoginState);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isLoggedIn) {
-      setIsModalOpen(true);
-      setTimeout(() => {
-        setIsModalOpen(false);
-        navigate('/login');
-      }, 2500);
-    }
-  }, [isLoggedIn]);
-
-  if (!isLoggedIn) {
-    return (
-      <>
-        <div
-          className={`fixed top-0 left-0 w-full h-full flex pt-[131.2px] justify-center ${
-            isModalOpen ? 'flex' : 'hidden'
-          } bg-opacity-20 backdrop-blur text-white text-2xl z-50`}
-        >
-          <div className="h-[80px] w-[312px] flex justify-center items-center rounded-md text-[18px] bg-[#EDF1F8] text-[#4771B7] font-semibold">
-            접근권한이 없습니다.
-          </div>
-        </div>
-        {component}
-      </>
-    );
-  }
-
-  return component;
-};
-
 function MyRouter() {
   return (
     <Routes>
       <Route element={<HeaderLayout />}>
         <Route element={<FooterLayout />}>
           <Route element={<SideBarLayout />}>
-            <Route path="/my" element={requireLogin(<MyPage />)} />
-            <Route path="/my/wish" element={requireLogin(<WishList />)} />
-            <Route path="/my/order" element={requireLogin(<ReservationCheck />)} />
-            <Route path="/my/stores" element={requireLogin(<StoreCheck />)} />
+            <Route path="/my" element={<MyPage />} />
+            <Route path="/my/wish" element={<WishList />} />
+            <Route path="/my/order" element={<ReservationCheck />} />
+            <Route path="/my/stores" element={<StoreCheck />} />
           </Route>
-          <Route path="/my/order/edit" element={requireLogin(<ReservationModify />)} />
-          <Route path="/store/add" element={requireLogin(<StoreAdd />)} />
-          <Route path="/store/edit" element={requireLogin(<StoreAdd />)} />
+          <Route path="/my/order/edit" element={<ReservationModify />} />
+          <Route path="/store/add" element={<StoreAdd />} />
+          <Route path="/store/edit" element={<StoreAdd />} />
         </Route>
       </Route>
     </Routes>
