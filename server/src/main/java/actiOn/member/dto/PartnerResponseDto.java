9a14c1239367b1aecda44f2e86b1924a81a22948
isu-nice <isu010614@naver.com>
2023-07-23 13:51:52 +0900

    [chore] dto 수정

--- server/src/main/java/actiOn/member/dto/PartnerResponseDto.java
+++ server/src/main/java/actiOn/member/dto/PartnerResponseDto.java
@@ -1,5 +1,6 @@
 package actiOn.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,12 +10,14 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class PartnerResponseDto {
     private List<StoreDto> stores;
 
     @Builder
     @Getter
     @Setter
+    @AllArgsConstructor
     public static class StoreDto {
         private String category;
         private String storeName;
