52b56b8acb041583b334e0b646678927875b4019
isu-nice <isu010614@naver.com>
2023-07-23 15:16:40 +0900

    [fix] 어노테이션 추가

--- server/src/main/java/actiOn/store/repository/StoreRepository.java
+++ server/src/main/java/actiOn/store/repository/StoreRepository.java
@@ -3,15 +3,17 @@ package actiOn.store.repository;
 import actiOn.store.entity.Store;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface StoreRepository extends JpaRepository<Store, Long> , StoreCustomRepository{
-//    List<Store> findByCategory(String category);
-
     List<Store> findAll(Sort sort);
+
     List<Store> findByCategory(String category, Sort sort);
 
     List<Store> findTop4ByOrderByLikeCountDesc();
-    List<Store> findByStoreNameContainingOrderByRatingDesc(String keyword);//
+
+    List<Store> findByStoreNameContainingOrderByRatingDesc(String keyword);
 }
